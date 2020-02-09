da792fb47ea4cd2c7ff43d741743baec3119a40a
polich7 <phominykh1997@gmail.com>
2020-02-09 17:01:43 +0500

    убрала знаки препинания, посчитала частотность

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
 import pymorphy2
+import sys
+import string
+frequency = {}
 morph = pymorphy2.MorphAnalyzer()
 with open("vacancy.txt", "r", encoding = 'utf-8') as f:
     ls = list()  # запишем весь текст разбитый на слова
@@ -9,5 +12,12 @@ with open("vacancy.txt", "r", encoding = 'utf-8') as f:
             p = morph.parse(word)[0]  # делаем разбор
             words.append(p.normal_form)
         ls += words
-print(ls)
+ls = [word for word in ls if word.isalpha()]  # убираем знаки препинания
+frequencies = {}
+for word in ls:
+    frequencies[word] = frequencies.get(word, 0) + 1
+keys = sorted(frequencies.keys())
+for word in keys:
+    print(" %s, %d \n" % (word,frequencies[word]))
+
 
